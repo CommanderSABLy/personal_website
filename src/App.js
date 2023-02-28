845b474910a56f1d6822f68ea6b6750419ac1031
Saul Lynn <saullynn.lynn@gmail.com>
2023-02-27 23:47:18 -0600

    Implemented RWA test

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useState } from 'react';
+import { RWA } from './questions';
+import './quiz.css';
+
+function Quiz() {
+  const [activeQuestion, setActiveQuestion] = useState(0)
+  const [selectedAnswer, setSelectedAnswer] = useState('')
+  const [showResult, setShowResult] = useState(false)
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+  const { questions } = RWA
+  const { question, choices, scoring } = questions[activeQuestion]
+  const [result, setResult] = useState(0)
+
+  const nine_scale_asc = {
+    'Very Strongly Disagree': 1,
+    'Strongly Disagree': 2,
+    'Moderately Disagree': 3,
+    'Slightly Disagree': 4,
+    'Exactly Neutral': 5,
+    'Slightly Agree': 6,
+    'Moderately Agree': 7,
+    'Strongly Agree': 8,
+    'Very Strongly Agree': 9,
+  }
+
+  const nine_scale_des = {
+    'Very Strongly Disagree': 9,
+    'Strongly Disagree': 8,
+    'Moderately Disagree': 7,
+    'Slightly Disagree': 6,
+    'Exactly Neutral': 5,
+    'Slightly Agree': 4,
+    'Moderately Agree': 3,
+    'Strongly Agree': 2,
+    'Very Strongly Agree': 1,
+  }
+
+  const onClickNext = () => {
+    setResult((prev) => prev + selectedAnswer)
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1)
+    } else {
+      setActiveQuestion(0)
+      setShowResult(true)
+    }
+    setSelectedAnswer(null)
+    setSelectedAnswerIndex(null)
+  }
+
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index)
+    if (questions[activeQuestion].scoring === 'Ascend') {
+      setSelectedAnswer(nine_scale_asc[answer])
+    } else if (questions[activeQuestion].scoring === 'Descend') {
+      setSelectedAnswer(nine_scale_des[answer])
+    } else {
+      setSelectedAnswer(0)
+    }
+  }
+
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`)
+
+  return (
+    <div className="quiz-container">
+      {!showResult ? (
+        <div>
+          <div>
+          <span className="active-question-no">
+            {addLeadingZero(activeQuestion + 1)}
+          </span>
+          <span className="total-question">
+            /{addLeadingZero(questions.length)}
+          </span>
+        </div>
+        <h2>
+          {question}
+        </h2>
+
+        <ul>
+          {choices.map((answer, index) =>(
+            <li
+            onClick={() => onAnswerSelected(answer, index, scoring)}
+            key={answer}
+            className={selectedAnswerIndex === index ? 'selected-answer' : null}>
+            {answer}</li>
+          ))}
+        </ul>
+        <div className="flex-right">
+          <button onClick={onClickNext} disabled={selectedAnswerIndex === null}>{activeQuestion === questions.length - 1 ? 'Finish': 'Next'} </button>
+        </div>
+        </div>
+      ) : (
+        <div className="result">
+          <h3>Right Wing Authoritarianism Score</h3>
+          <p>
+            Total Score:<span> {100 * ((result - 30) /240)}%</span>
+          </p>
+          <p>
+            Total Questions: <span>{questions.length}</span>
+          </p>
+        </div>
+      )}
+      
+    </div>
+  );
+}
+
+export default Quiz;
---
+++ src/questions.js
@@ -0,0 +1,187 @@
+const nine_scale = [
+    'Very Strongly Disagree',
+    'Strongly Disagree',
+    'Moderately Disagree',
+    'Slightly Disagree',
+    'Exactly Neutral',
+    'Slightly Agree',
+    'Moderately Agree',
+    'Strongly Agree',
+    'Very Strongly Agree',
+]
+
+export const RWA = {
+    topic: 'Right Wing Authoritarianism',
+    questions: [
+        {
+            question: 'Life imprisonment is justified for certain crimes.',
+            choices: nine_scale,
+            scoring: 'None'
+        },
+        {
+            question: 'Women should have to promise to obey their husbands when they get married.',
+            choices: nine_scale,
+            scoring: 'None'
+        },
+        {
+            question: 'The established authorities in our country are usually smarter, better informed, and more competent than others are, and the people can rely on them.',
+            choices: nine_scale,
+            scoring: 'None'
+        },
+        {
+            question: 'It is important to protect fully the rights of radicals and deviants.',
+            choices: nine_scale,
+            scoring: 'None'
+        },
+        {
+            question: 'Our country desperately needs a mighty leader who will do what has to be done to destroy the radical new ways and sinfulness that are ruining us.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'Gays and lesbians are just as healthy and moral as anybody else.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'Our country will be great if we honor the ways of our forefathers, do what the authorities tell us to do, and get rid of the rotten apples who are ruining everything.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'Atheists and others who have rebelled against established religion are no doubt every bit as good and virtuous as those who attend church regularly.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'The real keys to the good life are obedience, discipline, and sticking to the straight and narrow.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'A lot of our rules regarding modesty and sexual behavior are just customs which are not necessarily any better or holier than those which other people follow.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'There are many radical, immoral people in our country today who are trying to ruin it for their own godless purposes, whom the authorities should put out of action.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'It is always better to trust the judgment of the proper authorities in government and religion than to listen to the noisy rabble-rousers in our society who are trying to create doubt in people\'s minds.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'There is absolutely nothing wrong with nudist camps.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'There is no one right way to live your life. Everybody has to create their own way.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'Our country will be destroyed someday if we do not smash the perversions eating away at our moral fiber and traditional beliefs.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'It\'s a mistake to stick strictly to the straight and narrow in life, for you\'ll miss a lot of interesting people from quite different backgrounds who can change you, and some of the best experiences you can have.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'The situation in our country is getting so serious, the strongest methods would be justified if they eliminated the troublemakers and got us back to our true path.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'It would be best for everyone if the proper authorities censored magazines so that people could not get their hands on trashy and disgusting material.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'Everyone should have their own lifestyle, religious beliefs, and sexual preferences, even if it makes them different from everyone else.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'A woman\'s place should be wherever she wants to be. The days when women are submissive to their husbands and social conventions belong strictly in the past.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'What our country really needs is a strong, determined leader who will crush evil and take us back to our true path.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'People should pay less attention to the Bible and the other old traditional forms of religious guidance and instead develop their own personal standards of what is moral and immoral.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'Enough is enough! If the loafers, deviants, and troublemakers won\'t shape up, then they should be severely disciplined and taught a lesson they\'ll never forget.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'Our country needs freethinkers who will have the courage to defy traditional ways, even if this upsets many people.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'There is nothing wrong with premarital sexual intercourse.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'It may be considered old-fashioned by some, but having a normal, proper appearance is still the mark of a gentleman and, especially, a lady.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'It is wonderful that young people today have greater freedom to protest against things they don\'t like and to make their own rules to govern their behavior.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'What our country really needs, instead of more civil rights, is a good stiff dose of law and order.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'Government, judges, and the police should never be allowed to censor books.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'Obedience and respect for authority are the most important virtues children should learn.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'We should treat protesters and radicals with open arms and open minds, since new ideas are the lifeblood of progressive change.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'Once our government leaders and the authorities condemn the dangerous elements in our society, it will be the duty of every patriotic citizen to help stomp out the rot that is poisoning our country from within.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+        {
+            question: 'Rules about being well-behaved and respectable should be changed in favor of greater freedom and new ways of living.',
+            choices: nine_scale,
+            scoring: 'Descend'
+        },
+        {
+            question: 'The facts on crime, sexual immorality, and recent public disorders all show we have to crack down harder on deviant groups and troublemakers if we are going to save our moral standards and preserve law and order.',
+            choices: nine_scale,
+            scoring: 'Ascend'
+        },
+    ]
+}
