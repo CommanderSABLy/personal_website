de4c670e2badabad22a89df32b19806cf90f9ccf
Saul Lynn <saullynn.lynn@gmail.com>
2023-03-12 19:12:05 -0500

    Fixed bugs in Gallery

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,44 +1,52 @@
 import React from "react";
 import './gallery.css';
+import { useState } from 'react';
 
 const Gallery = () => {
+    const [mouseDownAt, setMouseDownAt] = useState(0);
+    const [prevPercentage, setPrevPercentage] = useState(0);
+    const [percentage, setPercentage] = useState(0);
+    const [fullScreen, setFullScreen] = useState(false);
     document.body.style.backgroundImage = "none";
 
     if (window.location.pathname === '/gallery') {
         const track = document.getElementById("image-track");
     
-        if(typeof(track) != "undefined" && track != null) {
-            window.onmousedown = e => {
-                track.dataset.mouseDownAt = e.clientX;
+        window.onmousedown = e => {
+            setMouseDownAt(e.clientX);
+        }
+    
+        window.onmouseup = () => {
+            setMouseDownAt(0);
+            setPrevPercentage(percentage);
+            if(fullScreen) {
+                fullPage.style.backgroundImage = 'none';
+                fullPage.style.display = 'none';
+                setFullScreen(false);
             }
-        
-            window.onmouseup = () => {
-                track.dataset.mouseDownAt = "0";
-                track.dataset.prevPercentage = track.dataset.percentage;
-            }
-        
-            window.onmousemove = e => {
-                if(track.dataset.mouseDownAt === "0") return;
-        
-                var mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-                    maxDelta = window.innerWidth / 2;
-                
-                var percentage = (mouseDelta / maxDelta) * -100,
-                    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-                    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-                
-                
-                track.dataset.percentage = nextPercentage;
-        
-                track.animate({
-                    transform: `translate(${nextPercentage}%, -50%)`
+        }
+    
+        window.onmousemove = e => {
+            if(mouseDownAt === 0) return;
+    
+            var mouseDelta = mouseDownAt - e.clientX,
+                maxDelta = window.innerWidth / 2;
+            
+            var percentage = (mouseDelta / maxDelta) * -100,
+                nextPercentageUnconstrained = prevPercentage + percentage,
+                nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+            
+            
+            setPercentage(nextPercentage);
+    
+            track.animate({
+                transform: `translate(${nextPercentage}%, -50%)`
+            }, { duration: 1200, fill: "forwards" });
+    
+            for(const image of track.getElementsByClassName("image")) {
+                image.animate({
+                objectPosition: `${nextPercentage + 100}% center`
                 }, { duration: 1200, fill: "forwards" });
-        
-                for(const image of track.getElementsByClassName("image")) {
-                    image.animate({
-                    objectPosition: `${nextPercentage + 100}% center`
-                    }, { duration: 1200, fill: "forwards" });
-                }
             }
         }
     }
@@ -49,12 +57,9 @@ const Gallery = () => {
         img.addEventListener('dblclick', function() {
             fullPage.style.backgroundImage = 'url(' + img.src + ')';
             fullPage.style.display = 'block';
+            setFullScreen(true);
         })
     })
-    fullPage.addEventListener('click', function() {
-        fullPage.style.backgroundImage = 'none';
-        fullPage.style.display = 'none';
-    })/**/
 
     return (
         <div>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import './home.css'
 
 const Home = () => {
+    const resetBackground = () => {
+        {document.body.style.backgroundImage = 'url("https://fossbytes.com/wp-content/uploads/2022/06/spy-x-family-anya-smug-1024x576.jpg")'}
+      }
     return (
-        <div>
+        <div className='home'>
+            {resetBackground()}
             <h1>
                 Home page
             </h1>
